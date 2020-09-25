9e541873f24064aff0c32f3a0eed85a41ddc3410
Vladimir <groeter457028@gmail.com>
2020-09-25 11:47:57 +0000

    25.09_2

--- app/Services/Bitrix.php
+++ app/Services/Bitrix.php
@@ -8,12 +8,13 @@ class Bitrix
     public static function setLeadParameters($input)
     {
 
+// setLead and putLead defined in app\helpers.php
+
         $queryUrl = env('BITRIX_URL');
         $queryData = setLead($input);
         putLead($queryUrl,$queryData);
 
     }
-
 
 }
 
