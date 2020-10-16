550a88bd735fd906b6fa75ad543030145d0efd8d
Vladimir <groeter457028@gmail.com>
2020-10-16 13:46:46 +0000

    22

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\Request;
 use App\Member;
 use Illuminate\View\View;
-
+use App\Services\Bitrix;
 /**
  * Class ListController
  *
@@ -24,7 +23,9 @@ class ListController extends Controller
         if (view()->exists('layouts.list')) {
 
             $members = Member::all();
-            return view('layouts.list', array('members' => $members));
+            $newBitrix = new Bitrix();
+            $membersStatusUpdate = $newBitrix->checkLeadStatus($members);
+            return view('layouts.list', array('members' => $membersStatusUpdate));
 
         }
 
--- app/Services/Bitrix.php
+++ app/Services/Bitrix.php
@@ -66,6 +66,7 @@ class Bitrix
             }
 
         }
+
         return $speakers;
     }
 
